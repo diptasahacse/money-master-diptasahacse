eb5d3e4620d6d882a03a2b5aeb54ce8756f13fe4
diptasahacse <diptasaha.lpu.cse@gmail.com>
2022-02-17 16:39:29 +0600

    check valid input

--- js/script.js
+++ js/script.js
@@ -1,3 +1,9 @@
+/*
+Author: Dipta Saha
+Email: diptasaha.lpu.cse@gmail.com
+Description: This is Programming Hero 5th Assignment
+Date: 16th Feb 2022 
+*/
 function getInputValue(id) {
     const getNumberInput = Number(document.getElementById(id).value);
     return getNumberInput;
@@ -6,10 +12,22 @@ function getInputValue(id) {
 function setInnerText(id, value) {
     document.getElementById(id).innerText = value;
 }
-// check validity by using keyup listener
-function checkValidity(id) {
-    console.log('change');
+
+function setValue(id, value) {
+    document.getElementById(id).value = value;
 }
+// check validity by using keyup listener
+function checkNumberValidate(id) {
+    let numberValue = getInputValue(id);
+    if (isNaN(numberValue)) {
+        alert('Please input number value..!');
+        setValue(id, '');
+    } else if (numberValue < 0) {
+        alert('Please input positive number value..!');
+        setValue(id, '');
+    }
+}
+// Calculate Button Listener
 document.getElementById('calculate-button').addEventListener('click', function() {
     // Get Income
     const getIncome = getInputValue('income-input-field');
@@ -19,13 +37,21 @@ document.getElementById('calculate-button').addEventListener('click', function()
     const getClothesCost = getInputValue('clothes-input-field');
     // Total Expenses Calculate
     let totalExpense = getFoodCost + getRentCost + getClothesCost;
-    // set total Expense
-    setInnerText('total-expense', totalExpense);
     // Calculate Balance
     let balance = getIncome - totalExpense;
-    // Set Balance
-    setInnerText('balance', balance);
 
+    // Check if Total Expense is less than Balance or Not
+    if (totalExpense > balance) {
+        alert('Total Expense should not greater than Balance');
+        setValue('food-input-field', '');
+        setValue('rent-input-field', '');
+        setValue('clothes-input-field', '');
+    } else {
+        // set total Expense
+        setInnerText('total-expense', totalExpense);
+        // Set Balance
+        setInnerText('balance', balance);
+    }
 });
 //Saving Button Listener
 document.getElementById('saving-button').addEventListener('click', function() {
@@ -36,10 +62,17 @@ document.getElementById('saving-button').addEventListener('click', function() {
     // Calculate Saving Amount
     let savingAmount = (getIncome * getSavingPercent) / 100;
     let remainingBalance = getIncome - savingAmount;
-    // set Saving Amount
-    setInnerText('saving-amount', savingAmount);
-    // set Remaining Balance
-    setInnerText('remaining-balance', remainingBalance);
+    // Check saving amount is greater than remaining balance or not
+    if (savingAmount > remainingBalance) {
+        alert('Total Expense should not greater than Balance');
+        setValue('saving-input-field', '');
 
+    } else {
+        // set Saving Amount
+        setInnerText('saving-amount', savingAmount);
+        // set Remaining Balance
+        setInnerText('remaining-balance', remainingBalance);
+
+    }
 
 });
